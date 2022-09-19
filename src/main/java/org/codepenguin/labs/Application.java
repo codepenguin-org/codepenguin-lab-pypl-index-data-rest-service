bba5ab0afb2e4775611efe3c8f5674be20c0fe1e
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-09-19 15:08:44 -0500

    :memo: (docs) Added OpenAPI documentation.

--- src/main/java/org/codepenguin/labs/Application.java
+++ src/main/java/org/codepenguin/labs/Application.java
@@ -1,12 +1,33 @@
 package org.codepenguin.labs;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        final var title = "codepenguin-lab-pypl-index-data-rest-service";
+        final var description = "Spring Boot REST application to return the Pypl Indices Data.";
+        final var version = "v1.0.0";
+        final var license = "Creative Commons Attribution 3.0 Unported";
+        final var licenseFile = "LICENSE.md";
+        final var urlPattern = "https://github.com/codepenguin-org/codepenguin-lab-pypl-index-data-rest-service/blob/main/%s";
+        final var licenseUrl = urlPattern.formatted(licenseFile);
+        final var readmeUrl = urlPattern.formatted(licenseFile);
+        return new OpenAPI()
+                .info(new Info().title(title).description(description).version(version)
+                        .license(new License().name(license).url(licenseUrl)))
+                .externalDocs(new ExternalDocumentation().description("README").url(readmeUrl));
     }
 }
--- src/main/java/org/codepenguin/labs/pypl/controller/PyPlIndexDataController.java
+++ src/main/java/org/codepenguin/labs/pypl/controller/PyPlIndexDataController.java
@@ -1,5 +1,10 @@
 package org.codepenguin.labs.pypl.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.codepenguin.labs.pypl.model.dto.PyplTopIndexResponse;
 import org.codepenguin.labs.pypl.model.entity.PyplTopDatabaseIndex;
 import org.codepenguin.labs.pypl.model.entity.PyplTopIdeIndex;
@@ -24,22 +29,70 @@ public class PyPlIndexDataController {
         this.pyplTopIndexService = pyplTopIndexService;
     }
 
+    @Operation(summary = "Gets the current programming language indices.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Gets the indices.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PyplTopIndexResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @GetMapping("/language")
     public ResponseEntity<PyplTopIndexResponse<PyplTopProgrammingLanguageIndex>> getCurrentProgrammingLanguageIndices()
             throws PyplTopIndexException {
         return ResponseEntity.ok(pyplTopIndexService.getCurrentIndices(PyplTopProgrammingLanguageIndex.class));
     }
 
+    @Operation(summary = "Gets the current IDE indices.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Gets the indices.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PyplTopIndexResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @GetMapping("/ide")
     public ResponseEntity<PyplTopIndexResponse<PyplTopIdeIndex>> getCurrentIdeIndices() throws PyplTopIndexException {
         return ResponseEntity.ok(pyplTopIndexService.getCurrentIndices(PyplTopIdeIndex.class));
     }
 
+    @Operation(summary = "Gets the current ODE indices.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Gets the indices.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PyplTopIndexResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @GetMapping("/ode")
     public ResponseEntity<PyplTopIndexResponse<PyplTopOdeIndex>> getCurrentOdeIndices() throws PyplTopIndexException {
         return ResponseEntity.ok(pyplTopIndexService.getCurrentIndices(PyplTopOdeIndex.class));
     }
 
+    @Operation(summary = "Gets the current database indices.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Gets the indices.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PyplTopIndexResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     @GetMapping("/db")
     public ResponseEntity<PyplTopIndexResponse<PyplTopDatabaseIndex>> getCurrentDatabaseIndices()
             throws PyplTopIndexException {
